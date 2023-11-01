50cb287d0f706c35fe4aa45bf3a4648f533626de
kylewuzhere <kyvasquez1026@gmail.com>
2023-11-01 22:34:52 +1300

    add: stuff

--- WarriorWars/Warrior.cs
+++ WarriorWars/Warrior.cs
@@ -1,17 +1,51 @@
-﻿namespace WarriorWars
+﻿using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
 {
 	class Warrior
 	{
-		private int AllianceStartingHealth;
-		private int HordeStartingHealth;
+		private const int ALLIANCE_STARTING_HEALTH = 100; // fields; camelCase
+		private const int HORDE_STARTING_HEALTH = 100;
 
-		private Faction faction;
+		private Faction faction; // enum field
 
-		private int health;
+		private int health; // more fields
 		private string name;
 		private bool isAlive;
 
-		private Weapon weapon;
+		public bool IsAlive // properties; PascalCase
+		{
+			get // get; read-only
+			{
+				return isAlive;
+			}
+		}
+
+		private Weapon weapon; // more fields; Weapon is a datatype
 		private Armor armor;
+
+		public Warrior(string name, Faction faction)
+		{
+			this.name = name;
+			this.faction = faction;
+			isAlive = true;
+
+			switch (faction)
+			{
+				case Faction.Alliance:
+					weapon = new Weapon(faction);
+					armor = new Armor(faction);
+					health = ALLIANCE_STARTING_HEALTH;
+					break;
+				case Faction.Horde:
+					weapon = new Weapon(faction);
+					armor = new Armor(faction);
+					health = HORDE_STARTING_HEALTH;
+					break;
+				default:
+					break;
+			}
+		}
 	}
 }
