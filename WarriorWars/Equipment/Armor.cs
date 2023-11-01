4e62f38989ca9d6fd4d693c6dbfb9d415bb763de
kylewuzhere <kyvasquez1026@gmail.com>
2023-11-01 22:36:00 +1300

    add: constructors for each Faction

---
+++ WarriorWars/Equipment/Armor.cs
@@ -0,0 +1,35 @@
+﻿using WarriorWars.Enum;
+
+namespace WarriorWars.Equipment
+{
+	class Armor
+	{
+		private const int ALLIANCE_ARMOR_POINTS = 5;
+		private const int HORDE_ARMOR_POINTS = 5;
+
+		private int armorPoints;
+
+		public int ArmorPoints
+		{
+			get
+			{
+				return armorPoints;
+			}
+		}
+
+		public Armor(Faction faction)
+		{
+			switch (faction)
+			{
+				case Faction.Alliance:
+					armorPoints = ALLIANCE_ARMOR_POINTS;
+					break;
+				case Faction.Horde:
+					armorPoints = HORDE_ARMOR_POINTS;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
---
+++ WarriorWars/Equipment/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using WarriorWars.Enum;
+
+namespace WarriorWars.Equipment
+{
+	class Weapon
+	{
+		private const int ALLIANCE_DAMAGE = 5;
+		private const int HORDE_DAMAGE = 5;
+
+		private int damage;
+
+		public int Damage
+		{
+			get
+			{
+				return damage;
+			}
+		}
+
+		public Weapon(Faction faction)
+		{
+			switch (faction)
+			{
+				case Faction.Alliance:
+					damage = ALLIANCE_DAMAGE;
+					break;
+				case Faction.Horde:
+					damage = HORDE_DAMAGE;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
