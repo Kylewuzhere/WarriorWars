251d3e77a1d09d4b0e02b81a59012fe316e5803b
kylewuzhere <kyvasquez1026@gmail.com>
2023-11-02 20:01:47 +1300

    add: Tools static class to hold ColorfulAttackWriteLine and ColorfulWriteLine methods to add color to Console.WriteLine()

---
+++ WarriorWars/Tools.cs
@@ -0,0 +1,29 @@
+﻿using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+	static class Tools
+	{
+		public static void ColorfulAttackWriteLine(string text, Faction faction)
+		{
+			if (faction == Faction.Alliance)
+			{
+				Console.ForegroundColor = ConsoleColor.Cyan;
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Magenta;
+
+			}
+			Console.WriteLine(text);
+			Console.ResetColor();
+		}
+
+		public static void ColorfulWriteLine(string text, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.WriteLine(text);
+			Console.ResetColor();
+		}
+	}
+}
