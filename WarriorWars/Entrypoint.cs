9d9d7512121e3c5d77a19bb0444f574159ce9f7b
kylewuzhere <kyvasquez1026@gmail.com>
2023-11-02 19:57:55 +1300

    add: Entrypoint class; create Warrior instances; use Attack() method on both

--- WarriorWars/Entrypoint.cs
+++ WarriorWars/Entrypoint.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Warrior!");
+﻿using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+	class Entrypoint
+	{
+		static Random rng = new();
+
+		static void Main()
+		{
+			Warrior allianceWarrior = new("Alliance Warrior", Faction.Alliance);
+			Warrior hordeWarrior = new("Horde Warrior", Faction.Horde);
+
+			while (allianceWarrior.IsAlive && hordeWarrior.IsAlive)
+			{
+				if (rng.Next(0, 10) < 5)
+				{
+					allianceWarrior.Attack(hordeWarrior);
+				}
+				else
+				{
+					hordeWarrior.Attack(allianceWarrior);
+				}
+			}
+
+		}
+	}
+}
